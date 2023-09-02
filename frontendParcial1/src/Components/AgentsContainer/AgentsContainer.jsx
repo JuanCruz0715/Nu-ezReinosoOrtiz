3a3253d75bd7dff2d766b93fe097be6553fce8d0
JuanCruz0715 <juanchi97cruz@gmail.com>
2023-09-02 19:18:34 -0300

    agents

---
+++ frontendParcial1/src/Components/AgentsContainer/AgentsContainer.jsx
@@ -0,0 +1,27 @@
+import React, {useEffect, useState} from "react";
+import { getAgentes } from "../../Api";
+import { CardAgents } from "../CardAgents/CardAgents";
+
+export default function AgentsContainer() { 
+    const [agentes, setAgentes] = useState([])
+
+    useEffect(() => {
+      const  fetchData = async () => {
+        try {
+            const data = await getAgentes();
+            console.log(data)
+        setAgentes(data)
+        } catch (error) {
+            console.error('Error al obtener los Agentes', error);
+        }
+      }
+      fetchData();
+    }, [])
+   console.log("agentes",agentes)
+    return (
+        <div>
+            {agentes.map((agentes)=>(<CardAgents key={agentes.uuid} Data={agentes} />))}
+          
+        </div> //aca vamos a crear las tarjetas, con la cantidad de pj que tengamos
+      );
+}
