e3c7504921f98de1d10e17def84723aa8a6f0416
JuanCruz0715 <juanchi97cruz@gmail.com>
2023-09-12 17:04:22 -0300

    fuente login, etc

--- frontendParcial1/src/Api.js
+++ frontendParcial1/src/Api.js
@@ -2,36 +2,20 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://valorant-api.com/v1",
 });
-export  const getAgentes = async() => {
-    const url = "/agents"
-    const response = await instance.get(url);
-    return response.data.data;
-
-} 
-
-export const getGuns = async () => {
-    const url = "/weapons" 
-    const response = await instance.get(url);
-    return response.data.data;
-   
-
-}
-<<<<<<< HEAD
-export const getMaps = async () => {
-  const url = "/Maps";
+export const getAgentes = async () => {
+  const url = "/agents";
   const response = await instance.get(url);
   return response.data.data;
 };
 
-=======
+export const getGuns = async () => {
+  const url = "/weapons";
+  const response = await instance.get(url);
+  return response.data.data;
+};
 
 export const getMaps = async () => {
   const url = "/maps";
   const response = await instance.get(url);
   return response.data.data;
 };
->>>>>>> 8a89deb6e8b39f48cff3b0b9826532f376fed8bc
-
-
-
-
--- frontendParcial1/src/Components/NavBar/Navbar.jsx
+++ frontendParcial1/src/Components/NavBar/Navbar.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 import "./Navbar.css";
-<<<<<<< HEAD
-import { Link, NavLink } from "react-router-dom";
-=======
 import { Link, NavLink, useLocation } from 'react-router-dom';
 
->>>>>>> 8a89deb6e8b39f48cff3b0b9826532f376fed8bc
 import logocard1 from "../../../public/logocard1.png";
 
 export default function Navbar() {
@@ -17,13 +13,12 @@ export default function Navbar() {
     return location.pathname !== loginRoute;
   };
   return (
-<<<<<<< HEAD
     <nav className="Container-Navbar">
       <img src={logocard1} alt="Valorant" />
       <ul className="Card-Navbar">
         <li>
           <NavLink
-            to="/"
+            to="/home"
             className={({ isActive }) => (isActive ? "active" : "")}
           >
             Home
@@ -55,26 +50,5 @@ export default function Navbar() {
         </li>
       </ul>
     </nav>
-=======
-    shouldShowNavbar() && (
-      <nav className="Container-Navbar">
-        <img src={logocard1} alt="Valorant" />
-        <ul className="Card-Navbar">
-          <li>
-            <NavLink to='/Home' className={({ isActive }) => (isActive ? 'active' : '')}>Home</NavLink>
-          </li>
-          <li>
-            <NavLink to='/Agents' className={({ isActive }) => (isActive ? 'active' : '')}>Agents</NavLink>
-          </li>
-          <li>
-            <NavLink to='/Weapons' className={({ isActive }) => (isActive ? 'active' : '')}>Weapons</NavLink>
-          </li>
-          <li>
-            <NavLink to='/Maps' className={({ isActive }) => (isActive ? 'active' : '')}>Maps</NavLink>
-          </li>
-        </ul>
-      </nav>
-    )
->>>>>>> 8a89deb6e8b39f48cff3b0b9826532f376fed8bc
   );
 }
