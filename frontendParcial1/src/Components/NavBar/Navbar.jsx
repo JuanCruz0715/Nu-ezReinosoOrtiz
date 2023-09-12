8a89deb6e8b39f48cff3b0b9826532f376fed8bc
nicoortiz1 <77462897+nicoortiz1@users.noreply.github.com>
2023-09-12 09:38:02 -0300

    quito el nav de login

--- frontendParcial1/src/Components/NavBar/Navbar.jsx
+++ frontendParcial1/src/Components/NavBar/Navbar.jsx
@@ -1,25 +1,36 @@
 import React from "react";
 import "./Navbar.css";
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useLocation } from 'react-router-dom';
+
 import logocard1 from "../../../public/logocard1.png";
+
 export default function Navbar() {
+  const location = useLocation();
+
+  const loginRoute = '/';
+
+  const shouldShowNavbar = () => {
+    return location.pathname !== loginRoute;
+  };
   return (
-    <nav className="Container-Navbar">
-      <img  src={logocard1} alt="Valorant" />
-      <ul className="Card-Navbar">
-        <li>
-        <NavLink to='/Home' className={({ isActive }) => (isActive ? 'active' : '')}>Home</NavLink>
-        </li>
-        <li>
-        <NavLink to='/Agents'  className={({ isActive }) => (isActive ? 'active' : '')}>Agents</NavLink>
-        </li>
-        <li>
-        <NavLink to='/Weapons'  className={({ isActive }) => (isActive ? 'active' : '')}>Weapons</NavLink>
-        </li>
-        <li>
-        <NavLink to='/Maps'  className={({ isActive }) => (isActive ? 'active' : '')}>Maps</NavLink>
-        </li>
-      </ul>
-    </nav>
+    shouldShowNavbar() && (
+      <nav className="Container-Navbar">
+        <img src={logocard1} alt="Valorant" />
+        <ul className="Card-Navbar">
+          <li>
+            <NavLink to='/Home' className={({ isActive }) => (isActive ? 'active' : '')}>Home</NavLink>
+          </li>
+          <li>
+            <NavLink to='/Agents' className={({ isActive }) => (isActive ? 'active' : '')}>Agents</NavLink>
+          </li>
+          <li>
+            <NavLink to='/Weapons' className={({ isActive }) => (isActive ? 'active' : '')}>Weapons</NavLink>
+          </li>
+          <li>
+            <NavLink to='/Maps' className={({ isActive }) => (isActive ? 'active' : '')}>Maps</NavLink>
+          </li>
+        </ul>
+      </nav>
+    )
   );
 }
